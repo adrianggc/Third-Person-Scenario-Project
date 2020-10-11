68aa4a363014028dd3cfe255ee34c2ecdbfc975a
adrianggc <agavidiacruz@gmail.com>
2020-10-10 19:52:19 -0500

    Created contextual message triggered event, added player tag

--- Assets/Scripts/ContextualMessageTrigger.cs
+++ Assets/Scripts/ContextualMessageTrigger.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ContextualMessageTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public delegate void ContextualMessageTriggerAction();
 
-    // Update is called once per frame
-    void Update()
+    public static event ContextualMessageTriggerAction ContextualMessageTriggered;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+           if(ContextualMessageTriggered != null)
+           {
+                ContextualMessageTriggered.Invoke();
+           }
+        }
     }
 }
