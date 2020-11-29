66ecb92984099bfc0ce49e54a768b7df2a06b705
adrianggc <agavidiacruz@gmail.com>
2020-11-28 18:45:35 -0600

    Refactored our character controller to clarify

---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private float speed = 5f;
+    private float turnspeed = 70.0f;
+
+
+    private float horizontalInput;
+    private float forwardinput;
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardinput = Input.GetAxis("Vertical");
+        //Move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardinput);
+        //Rotates the car based in the horizontal input
+        transform.Rotate(Vector3.up * turnspeed * horizontalInput * Time.deltaTime);
+        /*speed++;
+        if (speed <= 20.0f)
+        {
+            speed = 20f;
+        }*/
+    }
+}
--- Assets/Scripts/RigidbodyCharacterCrontroller.cs
+++ Assets/Scripts/RigidbodyCharacterCrontroller.cs
@@ -12,10 +12,17 @@ public class RigidbodyCharacterCrontroller : MonoBehaviour
     private float maxSpeed = 2;
 
     [SerializeField]
+    [Tooltip("How fast the player turns. 0 = no turning, 1 instant snap turning")]
+    [Range(0, 1)]
+    private float turnSpeed = 0.1f;
+     
+    [SerializeField]
     private PhysicMaterial stoppingPhyMaterial, movingPhyMaterial;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
     private new Collider collider;
+
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -24,29 +31,70 @@ public class RigidbodyCharacterCrontroller : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vector3 cameraRelativeInputDirection = GetCameraRelativeInputDirection();
+        UpdatePhysicsMaterial();
+        Move(cameraRelativeInputDirection);
+        RotateToFaceInputDirection(cameraRelativeInputDirection);
+    }
+
+    /// <summary>
+    /// Turning the character to face the direction it wants to move in. 
+    /// </summary>
+    /// <param name="movementDirection">The direction the character is trying to move in.</param>
+    private void RotateToFaceInputDirection(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude > 0)
+        {
+            var targetRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Movesthe player in a direction based on its max speed and acceleration.
+    /// </summary>
+    /// <param name="moveDirection">The direction to move in.</param>
+    private void Move(Vector3 moveDirection)
+    {
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(moveDirection * accelecForce, ForceMode.Acceleration);
+        }
+    }
+
+    /// <summary>
+    /// Updates the phy material to a low friction option if the player is trying to move,
+    /// or higher friction option if they are trying to stop.
+    /// </summary>
+    private void UpdatePhysicsMaterial()
+    {
+        collider.material = input.magnitude > 0 ? movingPhyMaterial : stoppingPhyMaterial;
+    }
+
+    /// <summary>
+    /// Uses the input vector to create camera relative version
+    /// so the player can move basd on the camera's forward.
+    /// </summary>
+    /// <returns>returns the camera relative input direction</returns>
+
+    private Vector3 GetCameraRelativeInputDirection()
+    {
         var inputDirection = new Vector3(input.x, 0, input.y);
+        Vector3 cameraFlattenedForward = Camera.main.transform.forward;
+        cameraFlattenedForward.y = 0;
+        var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
 
-        if(inputDirection.magnitude > 0)
-        {
-            collider.material = movingPhyMaterial;
-
-        }
-        else
-        {
-            collider.material = stoppingPhyMaterial;
-        }
-        if(rigidbody.velocity.magnitude < maxSpeed)
-        {
-            rigidbody.AddForce(inputDirection * accelecForce, ForceMode.Acceleration);
-        }
-        
+        Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
+        return cameraRelativeInputDirection;
+       
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         input = context.ReadValue<Vector2>(); 
     }
-    /*private void Update()
+
+   /* private void Update()
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
